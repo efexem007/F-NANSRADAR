968ea8863be4d9cdc23ff1007510b0ac014ac33d
efexem007 <mefe0055@gmail.com>
2026-04-25 23:58:04 +0300

    fix(Layout): remove AuthContext references causing null error

--- frontend/src/components/layout/Layout.jsx
+++ frontend/src/components/layout/Layout.jsx
@@ -1,12 +1,8 @@
 import { Outlet } from 'react-router-dom'
 import { Navbar } from './Navbar'
 import { Footer } from './Footer'
-import { useContext } from 'react'
-import { AuthContext } from '../../context/AuthContext'
 
 const Layout = () => {
-  const { user } = useContext(AuthContext)
-
   return (
     <div className="min-h-screen bg-[#0a0e1a] text-slate-100 relative">
       <Navbar />
